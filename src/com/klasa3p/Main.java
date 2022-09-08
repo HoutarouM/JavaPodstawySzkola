7394c30acce3bf24c5788b2b07c928e6453c178a
HoutarouM <marian.horoshko@gmail.com>
2022-09-08 11:23:56 +0200

    While

--- src/com/klasa3p/Main.java
+++ src/com/klasa3p/Main.java
@@ -9,71 +9,34 @@ public class Main {
     public static void main(String[] args) {
         // wylosowanie liczby
 
-//        zmienne typy:
-//        - proste - int
-//        - generyczne - String
-
-//        random zakres <0, 1)
-//        wylosowana z zakresu 1, 100
-
 //        rzutowanie zawezajace - wyrzuca liczby po przecinku
         int randomN = (int) (Math.random() * 100 + 1);
 
-//        Typy proste:
-//        - float
-//        - double
-//        - boolean
-//        - char
-//        - bite
-//        - short
-//        - int
-//        - long
-
         int iNumber;
-        int counter = 0;
+        int counter = 1;
 
         boolean isComplete = false;
 
-//    iteracje
-//    zgadywanie 10 razy
-    for(int i = 0; i < 10; i++){
-        //        Wyczytywanie
         System.out.println("Write number: ");
         Scanner input = new Scanner(System.in);
         iNumber = input.nextInt();
 
-        if (iNumber == randomN) {
-            System.out.println("Congratulations!! " + counter + " is your score.");
-            isComplete = true;
-            break;
-        } else {
-//            wyrarzenie ? trawda : falsz
-            int sub = iNumber > randomN ? iNumber - randomN : randomN - iNumber;
+//    iteracje
+        while (iNumber != randomN) {
+            //        Wyczytywanie
+            System.out.println("Write number: ");
+            input = new Scanner(System.in);
+            iNumber = input.nextInt();
 
-//            wyrarzenie zwraca wartosc
-//            wyrarzenie nie zwraca wartosci
-
-//            dzielenie calkowite
-            sub /= 10;
-
-//            switch
-            System.out.println(
-                    switch (sub) {
-                        case 0 -> "Near";
-                        case 1 -> "Not bad";
-                        case 2 -> "Close";
-                        default -> "Try something else";
-                    });
-
-//            wyrarzenie zwraca wartosc
-//            instrukcje nie
+            if(iNumber > randomN){
+                System.out.println("Zaduza");
+            } else {
+                System.out.println("Zamala");
+            }
 
             counter++;
         }
-    }
 
-    if(!isComplete){
-        System.out.println("Try binary search.");
-    }
+        System.out.println("Congratulation");
     }
 }
