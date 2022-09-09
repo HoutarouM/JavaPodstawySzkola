2b60b79c133a3fefbd7cc3d229a4f3dda1cb660d
HoutarouM <maryan.goroshko@ukr.net>
2022-09-09 17:08:20 +0200

    second exercise

--- src/com/java/homework/Main.java
+++ src/com/java/homework/Main.java
@@ -1,8 +1,14 @@
 package com.java.homework;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("First exercise");
         firstExercise();
+
+        System.out.println("Second exercise");
+        secondExercise();
     }
 
     public static void firstExercise(){
@@ -11,5 +17,29 @@ public class Main {
                 System.out.print(i + " ");
             }
         }
+    }
+
+    public static void secondExercise(){
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+        int wordLength = word.length();
+
+        boolean isPalindrome = true;
+
+        for(int i = 0; i < wordLength; i++){
+            if(word.charAt(i) != word.charAt(wordLength - i - 1)){
+                isPalindrome = false;
+            }
+        }
+
+        if(isPalindrome) {
+            System.out.println("Word is palindrome :)");
+        } else {
+            System.out.println("Word isn't palindrome :(");
+        }
+    }
+
+    public static void thirdExercise(){
+
     }
 }
