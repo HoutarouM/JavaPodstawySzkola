be1d8932d4bd5f55aa8a0cf4f3dc6ba720408371
HoutarouM <marian.horoshko@gmail.com>
2022-09-15 08:46:30 +0200

    losowanie bez powtorzen do zbioru

--- src/com/klasa3p/Main.java
+++ src/com/klasa3p/Main.java
@@ -1,30 +1,30 @@
 package com.klasa3p;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 //        Losowanie 6 liczb
         System.out.println("Witaj na losowaniu 6 liczb");
-//       tablica wylosowanych liczb
-//       wartosci w tablicy typu prostego lub zlozonego
-//       w tablicy nie ma mozliwosci zmiany jej rozmiaru po jej deklaracji
-        int tab[] = new int[6];
 
-        for (int i = 0; i < tab.length; i++) {
-            tab[i] = (int) (Math.random() * 100 + 1);
+//        tablica wylosowanych liczb
+//        losowanie bez powtorzen
+//        kolekcje
+//        moga miec tylko typy generyczne(zlozone)
+//        po zadeklarowanie mozna usuwac i dodawac elementy
+
+//        zbiour
+//        HashSet niema indeksacji ale elementy sie nie powtarzaja
+//        zbior zazwyczaj zawiera elementy bez powtorzen
+//        zbior zazwycza nie am indeksowania elementow
+//        zbior ma metody
+        Set<Integer> tab = new HashSet<>();
+
+//        while bo liczby moga sie powtarzac i nie dodawac sie do zbioru
+        while (tab.size() < 6) {
+            tab.add((int) (Math.random() * 10 + 1));
         }
-
-//        klonowanie objektow
-//        inne imie dla objectu
-        int tab2[] = tab;
-//        nowy object z wartosciami istniejacego
-        int tab3[] = tab.clone();
-
-        System.out.println("tab");
-        System.out.println(tab);
-        System.out.println("tab2");
-        System.out.println(tab2);
-        System.out.println("tab3");
-        System.out.println(tab3);
 
 //        odpowiednik foreach
         for (int i : tab) {
