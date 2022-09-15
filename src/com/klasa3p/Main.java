7aec4ab357f00e82af0edf5517aab783e1f78136
HoutarouM <marian.horoshko@gmail.com>
2022-09-15 07:28:41 +0200

    lesson 2

--- src/com/klasa3p/Main.java
+++ src/com/klasa3p/Main.java
@@ -7,39 +7,6 @@ import java.util.Scanner;
 // zgadnij liczbe
 public class Main {
     public static void main(String[] args) {
-        // wylosowanie liczby
 
-//        rzutowanie zawezajace - wyrzuca liczby po przecinku
-        int randomN = (int) (Math.random() * 100 + 1);
-
-        int iNumber;
-        int counter = 0;
-
-        Scanner input;
-
-        do {
-            //        Wyczytywanie
-            System.out.println("Write number: ");
-            input = new Scanner(System.in);
-            iNumber = input.nextInt();
-
-            if (iNumber == randomN) {
-                break;
-            }
-
-            if (iNumber > randomN) {
-                System.out.println("Zaduza");
-            } else {
-                System.out.println("Zamala");
-            }
-
-            counter++;
-        } while (iNumber != randomN);
-
-        System.out.println("Congratulation you tried " + counter + " times.");
     }
 }
-/**
- * Komentarze typu java doc
- * wszystkie komentarze dotyczace documentacji programu
- */
