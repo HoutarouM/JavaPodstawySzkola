c2b18d51b092b2e9a0284affa22b094eb95f1062
HoutarouM <maryan.goroshko@ukr.net>
2022-09-15 18:05:07 +0200

    second exercise

--- src/com/klasa3p/Main.java
+++ src/com/klasa3p/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         firstExercise();
+
+        secondExercise();
     }
 
     public static void firstExercise() {
@@ -18,5 +20,28 @@ public class Main {
         for (int i = 0; i < tab.length; i++) {
             System.out.println(tab[i]);
         }
+    }
+
+    public static void secondExercise() {
+        System.out.println("Wpisz liczbe: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int[] tab = new int[40];
+
+        tab[0] = 0;
+        tab[1] = 1;
+
+        for (int i = 2; i < tab.length; i++) {
+            tab[i] = tab[i - 2] + tab[i - 1];
+        }
+
+        int res_index = Arrays.binarySearch(tab, n);
+
+        if (res_index < 0) {
+            res_index = res_index * (-1) - 1;
+        }
+
+        System.out.println("res: " + tab[res_index]);
     }
 }
