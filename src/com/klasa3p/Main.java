fc4b913a150f61c8c079a42ed6f9ec66b9ccd9ed
HoutarouM <marian.horoshko@gmail.com>
2022-09-15 08:27:38 +0200

    Klonowanie objektow

--- src/com/klasa3p/Main.java
+++ src/com/klasa3p/Main.java
@@ -6,11 +6,25 @@ public class Main {
         System.out.println("Witaj na losowaniu 6 liczb");
 //       tablica wylosowanych liczb
 //       wartosci w tablicy typu prostego lub zlozonego
+//       w tablicy nie ma mozliwosci zmiany jej rozmiaru po jej deklaracji
         int tab[] = new int[6];
 
         for (int i = 0; i < tab.length; i++) {
             tab[i] = (int) (Math.random() * 100 + 1);
         }
+
+//        klonowanie objektow
+//        inne imie dla objectu
+        int tab2[] = tab;
+//        nowy object z wartosciami istniejacego
+        int tab3[] = tab.clone();
+
+        System.out.println("tab");
+        System.out.println(tab);
+        System.out.println("tab2");
+        System.out.println(tab2);
+        System.out.println("tab3");
+        System.out.println(tab3);
 
 //        odpowiednik foreach
         for (int i : tab) {
