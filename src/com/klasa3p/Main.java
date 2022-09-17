25a1ad97b9b5bcdb48478862a70366ad0831fb47
HoutarouM <maryan.goroshko@ukr.net>
2022-09-17 11:55:19 +0200

    third exercise

--- src/com/klasa3p/Main.java
+++ src/com/klasa3p/Main.java
@@ -1,13 +1,20 @@
 package com.klasa3p;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        firstExercise();
+//        firstExercise();
 
-        secondExercise();
+//        secondExercise();
+
+
+        int[] tab = new int[100];
+
+        thirdExercise(tab);
     }
 
     public static void firstExercise() {
@@ -24,6 +31,7 @@ public class Main {
 
     public static void secondExercise() {
         System.out.println("Wpisz liczbe: ");
+
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
 
@@ -36,12 +44,44 @@ public class Main {
             tab[i] = tab[i - 2] + tab[i - 1];
         }
 
+
         int res_index = Arrays.binarySearch(tab, n);
+
 
         if (res_index < 0) {
             res_index = res_index * (-1) - 1;
+
+            if ((double) n / tab[res_index] < 0.75) {
+                res_index--;
+            }
         }
 
+
         System.out.println("res: " + tab[res_index]);
+    }
+
+    public static void thirdExercise(int[] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = (int) (Math.random() * 100 + 1);
+        }
+
+
+//        kolekcja liczb parzystych
+        Set<Integer> evenNumbers = new HashSet<>();
+//        kolekcja liczb nie parzystych
+        Set<Integer> oddNumbers = new HashSet<>();
+
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] % 2 == 0) {
+                evenNumbers.add(tab[i]);
+            } else {
+                oddNumbers.add(tab[i]);
+            }
+        }
+
+
+        System.out.println("Ilosc liczb parzystych: " + evenNumbers.size());
+
+        System.out.println("Ilosc liczb nie parzystych: " + oddNumbers.size());
     }
 }
